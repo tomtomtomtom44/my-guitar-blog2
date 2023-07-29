dea0676b96a529314512103d5b26b1840e265737
thomas.batard <thomas.batard@gmail.com>
2023-07-29 21:06:18 +0200

    a tester double utilisation component video pour youtube et cloudinary

--- app/components/Video.tsx
+++ app/components/Video.tsx
@@ -1,13 +1,17 @@
 type Props = {
-    id: string
+    id: string,
+    type?: string
 }
 
-export default function Video({ id }: Props) {
+export default function Video({ id, type }: Props) {
+    const type_computed = type ? type : "youtube"
+    const cloudinary_id = process.env.CLOUDINARY_ID
+    const url = (type_computed === "cloudinary") ? `https://player.cloudinary.com/embed/?public_id=${id}&cloud_name=${cloudinary_id}` : `https://www.youtube.com/embed/${id}` 
     return (
         <div className="aspect-w-16 aspect-h-9">
             <iframe
-                src={`https://www.youtube.com/embed/${id}`}
-                title="YouTube video player"
+                src={url}
+                title="Video player"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             />
         </div>
