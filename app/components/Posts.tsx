9a76b8b281efb901db95fee824fb453f5b8c2986
thomas.batard <thomas.batard@gmail.com>
2023-07-30 13:56:42 +0200

    test deploiement prod

--- app/components/Posts.tsx
+++ app/components/Posts.tsx
@@ -10,7 +10,7 @@ export default async function Posts() {
 
     return (
         <section className="mt-6 mx-auto max-w-2xl">
-            <h2 className="text-4xl font-bold dark:text-white/90">Blog</h2>
+            {/* <h2 className="text-4xl font-bold dark:text-white/90">Blog</h2> */}
             <ul className="w-full list-none p-0">
                 {posts.map(post => (
                     <ListItem key={post.id} post={post} />
